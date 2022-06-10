c6a25bf31523fc6d54bc93530edbe662a5521d22
Josef Grosch <josef.grosch@addepar.com>
2022-06-09 19:00:29 -0700

    Flushing out the examples and a EOD check point

--- Examples/Example07/src/main/java/com/addepar/example07/Example07.java
+++ Examples/Example07/src/main/java/com/addepar/example07/Example07.java
@@ -1,12 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
+
 
 package com.addepar.example07;
 
 /**
- *
+ * Demos how to search for a specific key(s)
+ * 
  * @author Josef Grosch < josef.grosch@addepar.com >
  */
 public class Example07 {
